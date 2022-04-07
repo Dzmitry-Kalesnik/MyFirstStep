3d266243323ea0e995ce1b5041090c93a0351b25
Dzmitry-Kalesnik <81297740+Dzmitry-Kalesnik@users.noreply.github.com>
2022-04-08 00:45:47 +0300

    add script js file

---
+++ js/script.js
@@ -0,0 +1,9 @@
+menu.onclic = function myFunction() {
+	var x = document.getElementById('myTopnav');
+	
+	if (x.className === "topnav") {
+		x.className += " responsive";
+	} else {
+		x.className = "topnav";
+	}
+}
